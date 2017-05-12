1761ab98b6c9a8c674d15f08e7c9d2755eb2d17f
chezzman <email@example.com>
2017-05-12 11:11:48 +0100

    adding nav and starting firebase auth

--- client/js/app.js
+++ client/js/app.js
@@ -11,13 +11,43 @@ function MainRouter($stateProvider, $urlRouterProvider){
     .state('contact', {
       url: '/contact',
       templateUrl: '/states/contact.html'
+    })
+    .state('logup', {
+      url: '/logup',
+      templateUrl: 'states/logup.html'
+    })
+    // .state('signup', {
+    //   url: '/signup',
+    //   templateUrl: 'states/signup.html'
+    // })
+    .state('rofile', {
+      url: '/profile',
+      templateUrl: '/states/profile.html',
+      resolve: {
+        currentAuth: [
+          'AuthFactory',
+          (AuthFactory) => {
+            return AuthFactory.$requireSignIn();
+          }
+        ]
+      }
     });
 
+  $urlRouterProvider.otherwise('/');
 
 
 }
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
 
+function AuthCatcher($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {
+    if (error === 'AUTH_REQUIRED'){
+      $state.go('home');
+    }
+  });
+}
 angular
     .module('DisApp', ['ui.router', 'firebase'])
     .config(MainRouter)
     .constant('API_URL', 'http://localhost:3000')
+    .run(AuthCatcher);
